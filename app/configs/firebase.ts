aac63278d1fd8cb9817d77b1baa3b88d9a916594
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-19 16:33:48 +0300

    Add package json command to deploy firebase project

---
+++ app/configs/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCh3OWtGtNgJjPM0yAbaoLPrTRbgb6LLCQ",
+  authDomain: "fir-websockets-chat.firebaseapp.com",
+  projectId: "fir-websockets-chat",
+  storageBucket: "fir-websockets-chat.appspot.com",
+  messagingSenderId: "182207462801",
+  appId: "1:182207462801:web:bbda75e8afb64485026e30",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
